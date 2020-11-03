0857af11b7e5a424493e53abe761e96db62daf62
kennethkinyera <kennethkinyera55@gmail.com>
2020-11-03 21:14:50 +0000

    commit

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -3,34 +3,55 @@ export default class Search{
     //select DOM elements and useful data
 constructor(){
 
-  //this.injectHTML()
+  this.injectHTML()
   this.headerSelectIcon=document.querySelector(".header-search-icon")
   this.overlay=document.querySelector(".search-overlay")
   this.closeIcon=document.querySelector(".close-live-search")
+  this.inputField=document.querySelector("#live-search-field")
+  this.resultsArea=document.querySelector(".live-search-results")
+  this.loaderIcon=document.querySelector(".circle-loader")
+  this.typingwaittimer
+  this.previousValue=""
   this.events()
   
 }
 
   // events
 events(){
+    this.inputField.addEventListener("keyup",()=>this.keyPressHandler())
     this.closeIcon.addEventListener("click",()=> this.closeOverlay())
     this.headerSelectIcon.addEventListener("click",(e)=>{
         e.preventDefault()
-        console.log('yeahhhh')
         this.openOverlay()
     })
 }
   //methods
-  openoverlay(){
-    alert("scherch")
-    //this.overlay.classList.add("search-overlay--visible")
+  keyPressHandler(){
+
+      let value=this.inputField.value
+      if(value !="" && value !=this.previousValue){
+          clearTimeout(this.typingwaittimer)
+          this.showLoaderIcon()
+          this.typingwaittimer.setTimeout(()=>this.sendRequest(),3000)
+      }
+      this.previousValue=value
+  }
+  sendRequest(){
+      alert("stand up")
+  }
+  showLoaderIcon(){
+      this.loaderIcon.classList.add("circle-loader--visible")
+  }
+  openOverlay(){
+    this.overlay.classList.add("search-overlay--visible")
+    setTimeout(()=>this.inputField.focus(),50)
   }
   closeOverlay(){
     this.overlay.classList.remove("search-overlay--visible")
   }
 injectHTML(){
-    document.body.insertAdjacentHTML(`beforeend`,`
-  <div class="search-overlay">
+    document.body.insertAdjacentHTML('beforeend',
+`<div class="search-overlay ">
     <div class="search-overlay-top shadow-sm">
       <div class="container container--narrow">
         <label for="live-search-field" class="search-overlay-icon"><i class="fas fa-search"></i></label>
@@ -42,7 +63,7 @@ injectHTML(){
     <div class="search-overlay-bottom">
       <div class="container container--narrow py-3">
         <div class="circle-loader"></div>
-        <div class="live-search-results live-search-results--visible">
+        <div class="live-search-results ">
           <div class="list-group shadow-sm">
             <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
 
@@ -66,8 +87,5 @@ injectHTML(){
         </div>
       </div>
     </div>
-  </div>
-  `)
-}
-
+</div>`)}
 }

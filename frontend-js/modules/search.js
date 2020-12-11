db39e44537fd42a065904d207cd49e1e127582b6
kennethkinyera <kennethkinyera55@gmail.com>
2020-12-11 20:53:20 +0000

    commit

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -5,7 +5,6 @@ export default class Search{
     //select DOM elements and useful data
 constructor(){
 
-  //alert('constructors')
   this.injectHTML()
   this.headerSelectIcon=document.querySelector(".header-search-icon")
   this.overlay=document.querySelector(".search-overlay")
@@ -21,7 +20,7 @@ constructor(){
 
   // events
 events(){
-    //alert('events')
+    
     this.inputField.addEventListener("keyup",()=>this.keyPressHandler())
     this.closeIcon.addEventListener("click",()=> this.closeOverlay())
     this.headerSelectIcon.addEventListener("click",(e)=>{
@@ -32,7 +31,6 @@ events(){
   //methods
   keyPressHandler(){
 
-      //alert('keyPressHandler')
       let value=this.inputField.value
 
       if(value==""){
@@ -51,22 +49,20 @@ events(){
       this.previousValue=value
   }
   sendRequest(){
-      //alert('sendRequest')
+
       axios.post('/search',{searchTerm:this.inputField.value}).then(response=>{
 
         //alert(JSON.stringify(response.data))
         this.renderResultsHTML(response.data)
       }).catch(()=>{
           alert('failed')
-          //console.log('failed',"failed")
       })
   }
   renderResultsHTML(posts){
       //alert('renderResultsHTML')
     if(posts.length){
-        //alert(posts.length)
-        //this.resultsArea.innerHTML=`<p class="alert alert-danger text-center shadow-sm">Yes,uuuuwwwwee uuwwweeeee</p>`
-        /*this.resultsArea.innerHTML=`<div class="list-group shadow-sm">
+        
+        this.resultsArea.innerHTML=`<div class="list-group shadow-sm">
             <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
 
             <a href="#" class="list-group-item list-group-item-action">
@@ -85,10 +81,8 @@ events(){
               <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
               <span class="text-muted small">by brad on 0/12/2019</span>
             </a>
-          </div>`*/
+          </div>`
 
-          
-          alert(this.resultsArea)
     }
     else{
        this.resultsArea.innerHTML=`<p class="alert alert-danger text-center shadow-sm">Sorry,we couldn't find any results</p>`
@@ -103,10 +97,10 @@ events(){
       this.loaderIcon.classList.add("circle-loader--visible")
   }
   showResultsArea(){
-      this.loaderIcon.classList.add("live-search-results--visible")
+      this.resultsArea.classList.add("live-search-results--visible")
   }
   hideResultsArea(){
-      this.loaderIcon.classList.remove("live-search-results--visible")
+      this.resultsArea.classList.remove("live-search-results--visible")
   }
   openOverlay(){
     this.overlay.classList.add("search-overlay--visible")
@@ -129,31 +123,7 @@ injectHTML(){
     <div class="search-overlay-bottom">
       <div class="container container--narrow py-3">
         <div class="circle-loader"></div>
-        <div class="live-search-results">
-        
-        <div class="list-group shadow-sm">
-            <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
-
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
-              <span class="text-muted small">by barksalot on 0/14/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
-              <span class="text-muted small">by brad on 0/12/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
-              <span class="text-muted small">by barksalot on 0/14/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
-              <span class="text-muted small">by brad on 0/12/2019</span>
-            </a>
-          </div>
-
-
-        </div>
+        <div class="live-search-results">  </div>
       </div>
     </div>
 </div>`)}

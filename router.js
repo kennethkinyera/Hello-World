9b4f6b74e5007a606024b2a395b4782508b452e8
kennethkinyera <kennethkinyera55@gmail.com>
2020-12-16 11:56:49 +0000

    c

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const express=require("express")
 const router=express.Router()
 const userController=require("./controllers/userController")
 const postController=require("./controllers/postController")
+const followController=require("./controllers/followController")
 
 //user related posts
 router.get('/',userController.home)
@@ -21,4 +22,6 @@ router.post('/search',postController.search)
 //profile related routes
 router.get('/profile/:username',userController.ifUserExists,userController.profilePostsScreen)
 
+//follow related routes
+router.post('/addFollow/:username',userController.mustBeLoggedIn,followController.addFollow)
 module.exports=router

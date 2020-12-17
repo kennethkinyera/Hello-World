2d9f6108baefbaa719b52a0b3bed0751371bd2f5
kennethkinyera <kennethkinyera55@gmail.com>
2020-12-17 19:40:08 +0000

    c

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,7 +1,9 @@
 const Follow = require('../models/Follow')
 
 exports.addFollow = function(req, res) {
+
   let follow = new Follow(req.params.username, req.visitorId)
+  
   follow.create().then(() => {
     req.flash("success", `Successfully followed ${req.params.username}`)
     req.session.save(() => res.redirect(`/profile/${req.params.username}`))

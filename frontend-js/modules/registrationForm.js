b6abef828d83b2cf7ba332e4f824a2d252a1c640
kennethkinyera <kennethkinyera55@gmail.com>
2020-12-26 07:40:28 +0000

    commit

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -5,6 +5,8 @@ export default class RegistrationForm{
         this.insertValidationElements()
         this.username=document.querySelector("#username-register")
         this.username.previousValue=""
+        this.email=document.querySelector("#email-register")
+        this.email.previousValue=""
         this.events()
     }
 
@@ -13,6 +15,10 @@ export default class RegistrationForm{
     events(){
         this.username.addEventListener("keyup",()=>{
             this.isDifferent(this.username,this.usernameHandler)
+        })
+
+        this.email.addEventListener("keyup",()=>{
+            this.isDifferent(this.email,this.emailHandler)
         })
     }
     //methods
@@ -26,7 +32,18 @@ export default class RegistrationForm{
         this.username.errors=false
         this.usernameImmediately()
         clearTimeout(this.username.timer)
-        this.username.timer =setTimeout(()=>this.usernameAfterDelay(),3000)
+        this.username.timer =setTimeout(()=>this.usernameAfterDelay(),800)
+    }
+    emailHandler(){
+        this.email.errors=false
+        clearTimeout(this.email.timer)
+        this.email.timer =setTimeout(()=>this.emailAfterDelay(),800)
+    }
+
+    emailAfterDelay(){
+        if(! /^\S+@\S+$/.test(this.email.value)){
+              this.showValidationError(this.email,"You must provide a valid email address")
+        }
     }
     usernameImmediately(){
         if(this.username.value != "" && ! /^([a-zA-Z0-9]+)$/.test(this.username.value)){
